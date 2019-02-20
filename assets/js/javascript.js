9d503806ab9a5d554df96b4d4858f9f438455c26
Kevin Yeh <kevinyeh@Kevins-MacBook-Pro.local>
2019-02-20 12:40:33 -0800

    hid scrollbar display on sidebar. added js statement to detect mobile browers

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -44,3 +44,18 @@ $('.sidebar-links').on('click', function() {
     openSidebar();
   }
 });
+
+// const child = $('.sidebar-content');
+
+// child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";
+
+if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+) {
+  $('.sidebar-content').css('height', '80%');
+}
